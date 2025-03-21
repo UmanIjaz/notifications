ad2991c566af098ef61017dbb68e887e13a55759
umanijaz5 <umanijaz5@gmail.com>
2025-03-22 00:56:43 +0500

    style complete

--- src/App.js
+++ src/App.js
@@ -1,95 +1,161 @@
-import "./styles/App.css";
+import "./styles/index.css";
 import notifications from "./data.js";
 
 function App() {
-  return <NotificationsList notifications={notifications} />;
+  return (
+    <section id="wrapper">
+      <NotificationsList notifications={notifications} />
+    </section>
+  );
 }
 export default App;
 
 function NotificationsList({ notifications }) {
-  return notifications.map((notification) => {
-    return (
-      <NotificationItem key={notification.id} notification={notification} />
-    );
-  });
+  return (
+    <div className="notificationsList">
+      <div className="header">
+        <div className="header-content">
+          <h1>Notifications</h1>
+          <span>3</span>
+        </div>
+        <button>Mark all as read</button>
+      </div>
+      {notifications.map((notification) => (
+        <NotificationItem key={notification.id} notification={notification} />
+      ))}
+    </div>
+  );
 }
 
 function NotificationItem({ notification }) {
   return (
-    <div>
-      <userAvtar img={notification.img} />
-      <NotificationContent
-        type={notification.type}
-        userName={notification.user}
-        time={notification.time}
-      />
+    <div
+      className="notificationItem"
+      style={
+        !notification.read
+          ? { background: "hsl(205.71deg 63.64% 97.84%)" }
+          : null
+      }
+    >
+      <UserAvatar img={notification.img} />
+      <div>
+        <NotificationContent
+          type={notification.type}
+          userName={notification.user}
+          time={notification.time}
+          read={notification.read}
+        />
+        {notification.type === "private_message" && (
+          <MessagePrev content={notification.content} />
+        )}
+      </div>
     </div>
   );
 }
 
-function userAvtar(img) {
+function UserAvatar({ img }) {
   return (
-    <div>
-      <img src={img} />
+    <div className="userAvatar">
+      <img src={img} className="user-avtar" alt="User Avatar" />
     </div>
   );
 }
 
-function NotificationContent({ type, userName, time }) {
+function MessagePrev({ content }) {
+  return <div className="messagePrev">{content}</div>;
+}
+
+function NotificationContent({ type, userName, time, read }) {
   let message;
 
   switch (type) {
     case "reaction":
       message = (
-        <>
-          {userName} reacted to your recent post{" "}
+        <span>
+          <span>
+            <strong className="user-name">{userName}</strong> reacted to your
+            post{" "}
+          </span>
           <strong style={{ color: "hsl(219, 85%, 26%)" }}>
             My first tournament today
           </strong>
-        </>
+        </span>
       );
       break;
 
     case "follow":
-      message = <>{userName} followed you</>;
+      message = (
+        <span>
+          <span>
+            <strong className="user-name">{userName}</strong> followed you
+          </span>
+        </span>
+      );
       break;
 
     case "group_join":
       message = (
-        <>
-          {userName} has joined your group{" "}
+        <span>
+          <span>
+            <strong className="user-name">{userName}</strong> has joined your
+            group{" "}
+          </span>
           <strong style={{ color: "hsl(219, 85%, 26%)" }}>
             My first tournament today
           </strong>
-        </>
+        </span>
       );
       break;
 
     case "private_message":
-      message = <>{userName} sent you a private message</>;
+      message = (
+        <span>
+          <span>
+            <strong className="user-name">{userName}</strong> sent you a private
+            message
+          </span>
+        </span>
+      );
       break;
 
     case "comment":
-      message = <>{userName} commented on your post</>;
+      message = (
+        <span>
+          <span>
+            <strong className="user-name">{userName}</strong> commented on your
+            post
+          </span>
+        </span>
+      );
       break;
 
     case "group_leave":
       message = (
-        <>
-          {userName} left the group <strong>Class group</strong>
-        </>
+        <span>
+          <span>
+            <strong className="user-name">{userName}</strong> left the group{" "}
+          </span>
+          <strong>Class group</strong>
+        </span>
       );
       break;
 
     default:
-      message = <>There are no notifications for you</>;
+      message = (
+        <span>
+          <span>There are no notifications for you</span>
+        </span>
+      );
       break;
   }
 
   return (
-    <div>
-      {message}
-      <p style={{ color: "hsl(210, 60%, 98%)" }}>{time}</p>
+    <div className="notificationContent">
+      <p>
+        {message}
+        {!read && <span className="redDot"></span>}
+      </p>
+      <p>{time}</p>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const notifications = [
   {
     id: 1,
     user: "Mark Webber",
-    img: "/public/assets/avatar-mark-webber.webp",
+    img: "/assets/images/avatar-mark-webber.webp",
     type: "reaction",
     content: "My first tournament today!",
     time: "1m ago",
@@ -11,7 +11,7 @@ const notifications = [
   {
     id: 2,
     user: "Angela Gray",
-    img: "/public/assets/images/avatar-angela-gray.webp",
+    img: "/assets/images/avatar-angela-gray.webp",
     type: "follow",
     content: null,
     time: "5m ago",
@@ -20,7 +20,7 @@ const notifications = [
   {
     id: 3,
     user: "Jacob Thompson",
-    img: "/public/assets/avatar-jacob-thompson.webp",
+    img: "/assets/images/avatar-jacob-thompson.webp",
     type: "group_join",
     content: "Chess Club",
     time: "1 day ago",
@@ -29,7 +29,7 @@ const notifications = [
   {
     id: 4,
     user: "Rizky Hasanuddin",
-    img: "/public/assets/avatar-rizky-hasanuddin.webp",
+    img: "/assets/images/avatar-rizky-hasanuddin.webp",
     type: "private_message",
     content:
       "Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.",
@@ -39,7 +39,7 @@ const notifications = [
   {
     id: 5,
     user: "Kimberly Smith",
-    img: "/public/assets/avatar-kimberly-smith.webp",
+    img: "/assets/images/avatar-kimberly-smith.webp",
     type: "comment",
     content: "your picture",
     time: "1 week ago",
@@ -48,7 +48,7 @@ const notifications = [
   {
     id: 6,
     user: "Nathan Peterson",
-    img: "/public/assets/avatar-nathan-peterson.webp",
+    img: "/assets/images/avatar-nathan-peterson.webp",
     type: "reaction",
     content: "5 end-game strategies to increase your win rate",
     time: "2 weeks ago",
@@ -57,7 +57,7 @@ const notifications = [
   {
     id: 7,
     user: "Anna Kim",
-    img: "/public/assets/avatar-anna-kim.webp",
+    img: "/assets/images/avatar-anna-kim.webp",
     type: "group_leave",
     content: "Chess Club",
     time: "2 weeks ago",
